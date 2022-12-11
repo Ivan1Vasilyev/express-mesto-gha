87f2dfcf5836d62d39a33682c5eacec179b44d75
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-12-11 22:02:25 +0300

    try with body-parser

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-// const rateLimit = require('express-rate-limit');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const { celebrate, Joi, errors } = require('celebrate');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
@@ -19,15 +20,15 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 100,
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 app.use(helmet());
-// app.use(limiter);
+app.use(limiter);
 app.use(cookieParser());
-app.use(express.json());
+app.use(bodyParser.json());
 app.post(
   '/signin',
   celebrate({
