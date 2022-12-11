daf68c72d2c0c8f488f510c14a9b0aa7061c9bde
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-12-11 19:31:52 +0300

    auth fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,27 +4,24 @@ const NotAuthorizedError = require('../errors/not-authorized');
 const { JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  try {
-    const cookieJwt = req.cookies.jwt;
-    if (!cookieJwt || !cookieJwt.startsWith('Bearer ')) {
-      throw new NotAuthorizedError('Необходима авторизация');
-    }
-    const token = cookieJwt.replace(/^Bearer\s/, '');
-    let payload;
-
-    try {
-      payload = jwt.verify(token, JWT_SECRET);
-    } catch {
-      const err = new NotAuthorizedError('Необходима авторизация');
-      next(err);
-    }
-
-    req.user = { _id: payload._id };
-
-    next();
-  } catch (e) {
-    next(e);
+  const cookieJwt = req.cookies.jwt;
+  if (!cookieJwt) {
+    next(new NotAuthorizedError('Необходима авторизация'));
+    return;
   }
+  const token = cookieJwt.replace(/^Bearer\s/, '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (e) {
+    next(new NotAuthorizedError('Необходима авторизация'));
+    return;
+  }
+
+  req.user = { _id: payload._id };
+
+  next();
 };
 
 module.exports = auth;
