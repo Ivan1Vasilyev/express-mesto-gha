78b07390f28ca8129f8f6937ae71802c0342341e
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-12-15 18:24:13 +0300

    try test

--- controllers/users.js
+++ controllers/users.js
@@ -120,17 +120,13 @@ const login = async (req, res, next) => {
       return next(new NotAuthorizedError('Неправильные почта или пароль'));
     }
 
-    // const token = jwt.sign({ _id: '6398b291a0f2d689cee8fbf2' }, JWT_SECRET);
+    const token = jwt.sign({ _id: String(user._id) }, JWT_SECRET);
     return res
-      .cookie(
-        'jwt',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzk4YjI5MWEwZjJkNjg5Y2VlOGZiZjIiLCJpYXQiOjE2NzExMTc0MjJ9.qT9-lRwc1WDZZ3P_EJCIjpCnwlfziludrS6OywziHVY',
-        {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
-        },
-      )
+      .cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      })
       .json({ message: 'Токен jwt передан в cookie' });
   } catch (e) {
     return next(e);
