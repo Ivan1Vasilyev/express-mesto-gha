33bbbb146ab1619a34a308f79fd77e94560a3e04
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-12-15 15:55:48 +0300

    try test

--- models/users.js
+++ models/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
 const { REGEXP_URL } = require('../utils/constants');
+const NotAuthorizedError = require('../errors/not-authorized');
 
 const userSchema = new mongoose.Schema(
   {
@@ -41,5 +43,20 @@ const userSchema = new mongoose.Schema(
   },
   { versionKey: false },
 );
+
+userSchema.static('findUserByCredentials', async function (email, password, next) {
+  const user = await this.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new NotAuthorizedError('Неправильные почта или пароль'));
+  }
+
+  const isLogged = await bcryptjs.compare(password, user.password);
+  if (!isLogged) {
+    return next(new NotAuthorizedError('Неправильные почта или пароль'));
+  }
+  delete user.password;
+  console.log(user);
+  return user;
+});
 
 module.exports = mongoose.model('user', userSchema);
