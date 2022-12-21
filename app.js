bff3614f60ac870818cf2cb2e458e17f478948a7
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-12-21 20:17:03 +0300

    feat: логи добавлены

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { errors } = require('celebrate');
 const router = require('./routes');
 const { DEFAULT_ERROR, NOT_EXISTS_MESSAGE } = require('./utils/constants');
 const NotFoundError = require('./errors/not-found');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -24,10 +25,12 @@ app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
 app.use(express.json());
+app.use(requestLogger);
 app.use(router);
 app.use('*', (req, res, next) => {
   next(new NotFoundError(NOT_EXISTS_MESSAGE));
 });
+app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
